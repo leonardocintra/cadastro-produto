01dfffe98d5df6530fed2dfa1fa6e16d0053987f
Leonardo Cintra <leonardo.cintra@luizalabs.com>
2020-02-15 16:49:13 -0300

    remove and insert

--- api/controllers/departmentController.js
+++ api/controllers/departmentController.js
@@ -30,4 +30,33 @@ router.get('/', function (req, res, next) {
     });
 });
 
+/// DELETE
+route.delete('/:id', (req, res) => {
+    let id = req.params.id;
+    Department.deleteOne({ _id: id }, (err) => {
+        if (err) {
+            res.status(500);
+        } else {
+            res.status(200).send({});
+        }
+    });
+});
+
+/// UPDATE
+router.patch('/:id', (req, res) => {
+    Department.findById(req.params.id, (err, dep) => {
+        if (err) {
+            res.status(500).send(500);
+        } else if (!dep) {
+            res.status(404);
+        } else {
+            dep.name = req.body.name;
+            dep.save()
+                .then((d) => res.status(200).send(d))
+                .catch((e) => res.status(500).send(e));
+        }
+    });
+
+});
+
 module.exports = router;
