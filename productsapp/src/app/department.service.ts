aeba3d5f7ac59bc460429cb10c9641116709b916
Leonardo Cintra <leonardo.cintra@luizalabs.com>
2020-02-18 13:53:06 -0300

    edit e delete ok

--- productsapp/src/app/department.service.ts
+++ productsapp/src/app/department.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Department } from './department';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,51 @@ import { Observable } from 'rxjs';
 export class DepartmentService {
 
   readonly url = "http://localhost:3000/departments";
+  private departmentSubject$: BehaviorSubject<Department[]> = new BehaviorSubject<Department[]>(null);
+  private loadedDepartment: boolean = false;
 
   constructor(private http: HttpClient) { }
 
   get(): Observable<Department[]> {
-    return this.http.get<Department[]>(this.url);
+    if (!this.loadedDepartment) {
+      this.http.get<Department[]>(this.url)
+        .pipe(tap((deps) => {
+          console.log(deps);
+        }))
+        .subscribe(this.departmentSubject$);
+
+      this.loadedDepartment = true;
+    }
+
+    return this.departmentSubject$.asObservable();
   }
 
   add(department: Department): Observable<Department> {
-    return this.http.post<Department>(this.url, department);
+    return this.http.post<Department>(this.url, department)
+      .pipe(tap((dep: Department) => this.departmentSubject$.getValue().push(dep)))
+  }
+
+  del(department: Department): Observable<any> {
+    return this.http.delete(`${this.url}/${department._id}`)
+      .pipe(tap(() => {
+        let departments = this.departmentSubject$.getValue();
+        let i = departments.findIndex(d => d._id === department._id);
+        if (i >= 0) {
+          departments.splice(i, 1);
+        }
+      }));
+  }
+
+  update(department: Department): Observable<Department> {
+    return this.http.patch<Department>(`${this.url}/${department._id}`, department)
+      .pipe(
+        tap((dep) => {
+          let departments = this.departmentSubject$.getValue();
+          let i = departments.findIndex(d => d._id === department._id);
+          if (i >= 0) {
+            departments[i].name = dep.name;
+          }
+        })
+      );
   }
 }
--- productsapp/src/app/department/department.component.ts
+++ productsapp/src/app/department/department.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Department } from '../department';
 import { DepartmentService } from '../department.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-department',
@@ -11,31 +14,66 @@ export class DepartmentComponent implements OnInit {
 
   depName: string = '';
   departments: Department[] = [];
+  depEdit: Department = null;
 
-  constructor(private departmentService: DepartmentService) { }
+  private unsubiscribe$: Subject<any> = new Subject();
+
+  constructor(
+    private departmentService: DepartmentService,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
     this.departmentService.get()
+      .pipe(takeUntil(this.unsubiscribe$))
       .subscribe((deps) => this.departments = deps);
   }
 
   clearFields() {
     this.depName = "";
+    this.depEdit = null;
+  }
+
+  notify(message: string) {
+    this.snackBar.open(message, "OK", { duration: 3000 });
   }
 
   cancel() { }
 
   save() {
-    this.departmentService.add({
-      name: this.depName
-    }).subscribe((dep) => {
-      console.log(dep);
-      this.clearFields();
-    }, (err) => console.log(err));
+    if (this.depEdit) {
+      this.departmentService.update({
+        name: this.depName,
+        _id: this.depEdit._id
+      }).subscribe((dep) => {
+        this.notify("Atualizado com sucesso!");
+        this.clearFields();
+        this.depEdit = null;
+      }, (err) => {
+        this.notify("Error: " + err);
+      })
+    } else {
+      this.departmentService.add({
+        name: this.depName
+      }).subscribe((dep) => {
+        this.notify("Salvo com sucesso!");
+        this.clearFields();
+      }, (err) => console.log(err));
+    }
   }
 
-  edit(dep: Department) { }
+  edit(department: Department) {
+    this.depName = department.name;
+    this.depEdit = department;
+  }
 
-  delete(dep: Department) { }
+  delete(department: Department) {
+    this.departmentService.del(department)
+      .subscribe(() => this.notify("Removido"), (err) => console.log(err));
+  }
+
+  ngOnDestroy() {
+    this.unsubiscribe$.next();
+  }
 
 }
